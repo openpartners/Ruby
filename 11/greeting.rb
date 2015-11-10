973373d6ed14a1c24b5b2f26728611de2d21ef39
Andrey Sevostyanov <andrzejsew@gmail.com>
2015-11-10 10:52:50 +0100

    11 fix

--- 11/greeting.rb
+++ 11/greeting.rb
@@ -1,8 +1,10 @@
-def hello_word(family_name="domyślnyBond", first_name="domyślnyJames")
+def hello_word(first_name="domyślnyJames", family_name="domyślnyBond")
 	greeting = "hello world \n"
 	if family_name != "" && first_name != ""
 			greeting += "My name is " + first_name + " " + family_name
 	end
 	return greeting
 end
-puts hello_word("Bond", "James")
+puts hello_word
+puts hello_word("James")
+puts hello_word("James","Bond")
