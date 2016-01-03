f619384d8e24529fa1a662b5685c8ca8ece924e1
Andrey <andrzejsew@gmail.com>
2016-01-03 23:27:56 +0000

    4.7.1 Robot - filmy - export do CSV

--- 4.7/robot_imdb.com.rb
+++ 4.7/robot_imdb.com.rb
@@ -1,53 +1,35 @@
-require "mechanize"
+require 'mechanize'
+require 'csv'
 
+class Movie < Struct.new(:rating, :year, :title, :director ); end
+  
+movies = []
 agent = Mechanize.new
 
 main_page = agent.get 'http://www.imdb.com'
 list_page = main_page.link_with(text: "Top Rated Movies").click
 rows = list_page.root.css(".lister-list tr")
-# puts rows.size
-# wynik
-# 250
+
 rows.take(10).each do |row|
-title = row.at_css(".titleColumn a").text.strip # strip usunie spacje
-# puts title
+  title = row.at_css(".titleColumn a").text.strip # strip usunie spacje
+  rating = row.at_css(".ratingColumn strong").text.strip
+
+  movie_page = list_page.link_with(text: title).click
+
+  director = movie_page.root.at_css(".itemprop span").text.strip
+  year = movie_page.root.at_css("#titleYear a").text.strip
+
+  movie = Movie.new(rating, year, title, director)
+  movies << movie
+end
+
+puts movies.inspect
 # Wynik:
-# The Shawshank Redemption
-# The Godfather
-# The Godfather: Part II
-# The Dark Knight
-# 12 Angry Men
-# Schindler's List
-# Pulp Fiction
-# The Good, the Bad and the Ugly
-# The Lord of the Rings: The Return of the King
-# Fight Club
-rating = row.at_css(".ratingColumn strong").text.strip
-# puts "#{title}: #{rating}"
-# Wynik:
-# The Shawshank Redemption: 9.2
-# The Godfather: 9.2
-# The Godfather: Part II: 9.0
-# The Dark Knight: 8.9
-# 12 Angry Men: 8.9
-# Schindler's List: 8.9
-# Pulp Fiction: 8.9
-# The Good, the Bad and the Ugly: 8.9
-# The Lord of the Rings: The Return of the King: 8.9
-# Fight Club: 8.8
-movie_page = list_page.link_with(text: title).click
-director = movie_page.root.at_css(".itemprop span").text.strip
-year = movie_page.root.at_css("#titleYear a").text.strip
-puts "Rating: #{rating}, year: #{year}\t Title: #{title}:\t Director: #{director}" 
-# Wynik:
-# Rating: 9.2, year: 1994  Title: The Shawshank Redemption:        Director: Tim Robbins
-# Rating: 9.2, year: 1972  Title: The Godfather:   Director: Marlon Brando
-# Rating: 9.0, year: 1974  Title: The Godfather: Part II:  Director: Al Pacino
-# Rating: 8.9, year: 2008  Title: The Dark Knight:         Director: Christian Bale
-# Rating: 8.9, year: 1957  Title: 12 Angry Men:    Director: Martin Balsam
-# Rating: 8.9, year: 1993  Title: Schindler's List:        Director: Liam Neeson
-# Rating: 8.9, year: 1994  Title: Pulp Fiction:    Director: Tim Roth
-# Rating: 8.9, year: 1966  Title: The Good, the Bad and the Ugly:  Director: Eli Wallach
-# Rating: 8.9, year: 2003  Title: The Lord of the Rings: The Return of the King:   Director: Noel Appleby
-# Rating: 8.8, year: 1999  Title: Fight Club:      Director: Edward Norton
+# [#<struct Movie rating="9.2", year="1994", title="The Shawshank Redemption", director="Tim Robbins">, #<struct Movie rating="9.2", year="1972", title="The Godfather", director="Marlon Brando">, #<struct Movie rating="9.0", year="1974", title="The Godfather: Part II", director="Al Pacino">, #<struct Movie rating="8.9", year="2008", title="The Dark Knight", director="Christian Bale">, #<struct Movie rating="8.9", year="1957", title="12 Angry Men", director="Martin Balsam">, #<struct Movie rating="8.9", year="1993", title="Schindler's List", director="Liam Neeson">, #<struct Movie rating="8.9", year="1994", title="Pulp Fiction", director="Tim Roth">, #<struct Movie rating="8.9", year="1966", title="The Good, the Bad and the Ugly", director="Eli Wallach">, #<struct Movie rating="8.9", year="2003", title="The Lord of the Rings: The Return of the King", director="Noel Appleby">, #<struct Movie rating="8.8", year="1999", title="Fight Club", director="Edward Norton">]
+
+CSV.open("top10.csv", "w", col_sep: ";") do |csv|
+  csv << ["Ocena", "Rok produkcji", "Tytul filmu", "Rezyser"] # naglowki
+  movies.each do |movie|
+    csv << [movie.rating, movie.year, movie.title, movie.director]
+  end
 end
